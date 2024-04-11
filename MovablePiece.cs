8dc513524a3e99a2eb499899dbfb059f04a3b88a
Chris13136 <89609788+Chris13136@users.noreply.github.com>
2024-04-10 20:26:03 -0700

    Add files via upload

---
+++ MovablePiece.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoveablePiece : MonoBehaviour {
+
+    private GamePiece piece;
+
+    void Awake(){
+        piece = GetComponent<GamePiece> ();
+    }
+
+    void Start(){
+
+    }
+
+    void Update(){
+
+    }
+
+    public void Move(int newX, int newY){
+        piece.X = newX;
+        piece.Y = newY;
+
+        piece.transform.localPosition = piece.GridRef.GetWorldPosition(newX, newY)
+    }
+}
